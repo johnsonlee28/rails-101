a6169dd16efdc0febd7e529353f4ad5a19026b1d
kafu17173 <johnsonlee28@gmail.com>
2016-12-14 14:44:33 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,7 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
@@ -14,7 +15,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-
   end
 
 
@@ -33,7 +33,6 @@ class GroupsController < ApplicationController
 
   def update
 
-
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -42,10 +41,8 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-
     @group.destroy
-
-    redirect_to groups_path, alert: = "Group deleted"
+    redirect_to groups_path, alert: "Group deleted"
   end
 
 
