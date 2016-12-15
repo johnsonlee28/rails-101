e9dfbdd5c87bf936980ec98e7bc2596b5bd13242
kafu17173 <johnsonlee28@gmail.com>
2016-12-15 11:10:44 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,10 @@ Rails.application.routes.draw do
         get :hot
       end
     end
-
   end
   root 'groups#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :account do
+    resources :groups
+  end
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
